20dcb166f8e4a52ac18cbb5da11a45f6e15467f5
Juha Lindstedt <juha@pakastin.fi>
2020-07-14 23:05:31 +0300

    Lint

--- waveplus.js
+++ waveplus.js
@@ -1,9 +1,9 @@
-const noble = require("@abandonware/noble");
-const EventEmitter = require("events").EventEmitter;
+const noble = require('@abandonware/noble');
+const EventEmitter = require('events').EventEmitter;
 const struct = require('python-struct');
 
 class WavePlusDevice extends EventEmitter {
-  constructor(data) {
+  constructor (data) {
     super();
     this.id = data.id;
     this.serialNumber = data.serialNumber;
@@ -13,7 +13,7 @@ class WavePlusDevice extends EventEmitter {
 }
 
 class WavePlus extends EventEmitter {
-  constructor(serialNumber) {
+  constructor (serialNumber) {
     super();
     this.sn = serialNumber;
     this.uuid = ['b42e2a68ade711e489d3123b93f75cba'];
@@ -38,10 +38,10 @@ class WavePlus extends EventEmitter {
               id: peripheral.id,
               serialNumber: deviceInfo[1],
               address: peripheral.address,
-              connectable: peripheral.connectable,
+              connectable: peripheral.connectable
             });
             registerDevice(newWavePlus);
-            this.emit("found", newWavePlus);
+            this.emit('found', newWavePlus);
           }
         }
       }
@@ -64,15 +64,15 @@ class WavePlus extends EventEmitter {
               const SENSOR_IDX_CO2_LVL = 5;
               const SENSOR_IDX_VOC_LVL = 6;
 
-              this.sensorData[SENSOR_IDX_HUMIDITY] = rawData[1]/2.0;
+              this.sensorData[SENSOR_IDX_HUMIDITY] = rawData[1] / 2.0;
               this.sensorData[SENSOR_IDX_RADON_SHORT_TERM_AVG] = rawData[4];
               this.sensorData[SENSOR_IDX_RADON_LONG_TERM_AVG] = rawData[5];
-              this.sensorData[SENSOR_IDX_TEMPERATURE] = rawData[6]/100.0;
-              this.sensorData[SENSOR_IDX_REL_ATM_PRESSURE] = rawData[7]/50.0;
-              this.sensorData[SENSOR_IDX_CO2_LVL] = rawData[8]*1.0;
-              this.sensorData[SENSOR_IDX_VOC_LVL] = rawData[9]*1.0;
+              this.sensorData[SENSOR_IDX_TEMPERATURE] = rawData[6] / 100.0;
+              this.sensorData[SENSOR_IDX_REL_ATM_PRESSURE] = rawData[7] / 50.0;
+              this.sensorData[SENSOR_IDX_CO2_LVL] = rawData[8] * 1.0;
+              this.sensorData[SENSOR_IDX_VOC_LVL] = rawData[9] * 1.0;
 
-              wavePlus.emit("updated", {
+              wavePlus.emit('updated', {
                 rssi: peripheral.rssi,
                 humidity: this.sensorData[SENSOR_IDX_HUMIDITY],
                 temperature: this.sensorData[SENSOR_IDX_TEMPERATURE],
@@ -80,24 +80,24 @@ class WavePlus extends EventEmitter {
                 co2: this.sensorData[SENSOR_IDX_CO2_LVL],
                 voc: this.sensorData[SENSOR_IDX_VOC_LVL],
                 radonLtAvg: this.sensorData[SENSOR_IDX_RADON_LONG_TERM_AVG],
-                radonStAvg: this.sensorData[SENSOR_IDX_RADON_SHORT_TERM_AVG],
+                radonStAvg: this.sensorData[SENSOR_IDX_RADON_SHORT_TERM_AVG]
               });
             });
           });
-          peripheral.disconnect(function(error) {
+          peripheral.disconnect(function (error) {
             console.log('Disconnected from peripheral: ' + peripheral.uuid);
-         });
+          });
         });
-      };
+      }
     };
 
-    noble.on("discover", onDiscover);
+    noble.on('discover', onDiscover);
 
     // start scanning
-    if (noble.state === "poweredOn") {
+    if (noble.state === 'poweredOn') {
       noble.startScanning([], true);
     } else {
-      noble.once("stateChange", () => {
+      noble.once('stateChange', () => {
         noble.startScanning([], true);
       });
     }
